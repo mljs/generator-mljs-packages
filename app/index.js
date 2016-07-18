58356adce1a5ffa58f1f678b9012b87f387fc40a
Michaël Zasso <targos@protonmail.com>
2016-07-18 15:02:06 +0200

    feat: apply eslint to test directory

--- app/index.js
+++ app/index.js
@@ -57,6 +57,7 @@ module.exports = yeoman.Base.extend({
     this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
     this.fs.copy(this.templatePath('travis.yml'), this.destinationPath('.travis.yml'));
     this.fs.copy(this.templatePath('eslintrc.yml'), this.destinationPath('.eslintrc.yml'));
+    this.fs.copy(this.templatePath('eslintrc.test.yml'), this.destinationPath('test/.eslintrc.yml'));
     this.fs.write(this.destinationPath('src/index.js'), '\'use strict\';\n');
     this.fs.write(this.destinationPath('test/test.js'), '\'use strict\';\n');
 
